b3541f469173293af4deab659f26d726c18c21b5
elena.vanengelen <elena.vanengelen@edsn.nl>
2022-11-06 12:21:39 +0100

    tidy

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
 
 dependencies {
     implementation("software.amazon.awscdk:aws-cdk-lib:2.47.0")
-    implementation("software.amazon.awscdk:apigatewayv2-alpha:2.0.0-alpha.11")
-    implementation("software.amazon.awscdk:apigatewayv2-integrations-alpha:2.0.0-alpha.11")
     implementation("software.constructs:constructs:10.1.138")
 }
 
